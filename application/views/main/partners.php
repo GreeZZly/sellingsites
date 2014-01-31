2498873ce3d1302a6bd8290dbeec66134c55fbf1
Egor Kosolapov <greezzly7@gmail.com>
2014-01-31 17:39:14 +0400

    links partner

--- application/views/main/partners.php
+++ application/views/main/partners.php
@@ -1,7 +1,7 @@
 <div class="block_bg" id="partners_bg">
 	<div class="container" id="partners">
 		<div class="row block_title">Наши партнеры</div>
-		<div class="row partners_img"><img src="/include/images/4.png"></div>
-		<div class="row partners_img"><img src="/include/images/5.png"></div>
+		<div class="row partners_img"><a href="http://goodpractice.ru" target="_blank"><img src="/include/images/4.png"></a></div>
+		<div class="row partners_img"><a href="http://goodcrm.ru" target="_blank"><img src="/include/images/5.png"></a></div>
 	</div>
 </div>
