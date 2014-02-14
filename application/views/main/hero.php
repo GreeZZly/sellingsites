1940428c976ac3653877c0b62019a8c235519da9
Egor Kosolapov <greezzly7@gmail.com>
2014-02-14 18:28:47 +0400

    new hero

--- application/views/main/hero.php
+++ application/views/main/hero.php
@@ -1,12 +1,12 @@
 <div class="bloc_bg" id="hero_bg">
 	<div class="container" id="hero">
 		<div class="row">
-			<div class="span12" id="hero_msg_top">
+		<!-- 	<div class="span12" id="hero_msg_top">
 				БЕСПЛАТНЫЙ аудит вашего сайта
 			</div>
 			<div class="span12" id="hero_msg_bottom">
 				до 1 февраля
-			</div>
+			</div> -->
 
 		</div>
 	</div>
--- application/views/main/htmlheader.php
+++ application/views/main/htmlheader.php
@@ -3,8 +3,8 @@
   <head>
     <title>Продающие сайты | Selling Sites</title>
     <!-- Bootstrap -->
-    <link href="/favicon.png" rel="shortcut icon" type="image/x-icon" /
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400&subset=latin,cyrillic-ext,cyrillic,latin-ext' rel='stylesheet' type='text/css'>
+    <link href="/favicon.png" rel="shortcut icon" type="image/x-icon" />
+    <!-- <link href='http://fonts.googleapis.com/css?family=Roboto:400&subset=latin,cyrillic-ext,cyrillic,latin-ext' rel='stylesheet' type='text/css'> -->
     <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed&subset=latin,cyrillic-ext,cyrillic,latin-ext' rel='stylesheet' type='text/css'>
     <link href="/include/css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="/include/css/style.css" rel="stylesheet" media="screen">
