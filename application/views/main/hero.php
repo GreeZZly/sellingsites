875caa06ea8302523f4b019c1c7b0b02cd010856
Egor Kosolapov <greezzly7@gmail.com>
2014-06-10 17:57:36 +0400

    hero

--- application/views/main/hero.php
+++ application/views/main/hero.php
@@ -1,7 +1,7 @@
 <div class="bloc_bg" id="hero_bg">
 	<div class="container" id="hero">
 		<div class="row-fluid">
-			Интеграция с CRM
+			Контекстная реклама 
 
 		<!-- 	<div class="span12" id="hero_msg_top">
 				БЕСПЛАТНЫЙ аудит вашего сайта
@@ -11,7 +11,8 @@
 			</div> -->
 
 		</div>
-		<div class="row-fluid">БЕСПЛАТНО</div>
-		<div class="row-fluid">до 31 мая</div>
+		<div class="row-fluid">В ПОДАРОК</div>
+		<div class="row-fluid">до 30 июня</div>
 	</div>
 </div>
+
